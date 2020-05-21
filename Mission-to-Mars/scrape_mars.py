7aa51fffea493ff56ec755f5c9c0e9fc51c08eed
nvanlangen <59780892+nvanlangen@users.noreply.github.com>
2020-05-20 21:31:46 -0400

    updated web code

--- Mission-to-Mars/scrape_mars.py
+++ Mission-to-Mars/scrape_mars.py
@@ -8,46 +8,34 @@ def init_browser():
     executable_path = {"executable_path": "chromedriver.exe"}
     return Browser("chrome", **executable_path, headless=False)
 
+def visit_url(url,seconds,browser):
+    browser.visit(url)
+    time.sleep(seconds)
+    return browser.html
 
 def scrape():
     browser = init_browser()
     mars_dict = {}
 
-    url = "https://mars.nasa.gov/news/"
-
-    browser.visit(url)
-    time.sleep(5)
-    html = browser.html
-
-    soup = bs(html, 'html.parser')
+    soup = bs(visit_url("https://mars.nasa.gov/news/",5,browser), 'html.parser')
 
     results = soup.find('li', class_='slide')
     news_title = results.find('h3').text
     news_p = results.find('div',class_="article_teaser_body").text
     mars_dict.update({"title": news_title, "description": news_p})
 
-    url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-    browser.visit(url)
-    html = browser.html
-
-    soup = bs(html, 'html.parser')
+    soup = bs(visit_url("https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars",0,browser), 'html.parser')
     browser.click_link_by_id('full_image')
 
     browser.click_link_by_partial_text('more info')
 
-    html = browser.html
-    soup = bs(html, 'html.parser')
+    soup = bs(browser.html, 'html.parser')
 
     image_url = soup.find("img", class_="main_image")
-    featured_image_url = url.split("/spaceimages")[0] + image_url['src']
+    featured_image_url = browser.url.split("/spaceimages")[0] + image_url['src']
     mars_dict.update({"featimg" : featured_image_url})
 
-    url = "https://twitter.com/marswxreport?lang=en"
-    browser.visit(url)
-    time.sleep(20)
-    html = browser.html
-
-    soup = bs(html, 'html.parser')
+    soup = bs(visit_url("https://twitter.com/marswxreport?lang=en",20,browser), 'html.parser')
 
     articles = soup.find_all('article')
 
@@ -63,8 +51,7 @@ def scrape():
     
     mars_dict.update({"weather": mars_weather})
 
-    url = "https://space-facts.com/mars/"
-    tables = pd.read_html(url)
+    tables = pd.read_html("https://space-facts.com/mars/")
 
     df = tables[0]
     df.columns = ['Category', 'Value']
@@ -74,26 +61,25 @@ def scrape():
     html_table = html_table.replace('\n', '')
     mars_dict.update({"facts": html_table})
 
-    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-    browser.visit(url)
-    time.sleep(0)
-    html = browser.html
-
-    soup = bs(html, 'html.parser')
+    soup = bs(visit_url("https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars",0,browser), 'html.parser')
     items = soup.find_all('div',class_='description')
 
-    #loop thru the links and add dictionary to list
+    #loop thru the links and add to dictionary
+
+    img_title = []
     i = 1
     for item in items:
         link = item.find("a")
         title = link.text[0:len(link.text)-9]
         browser.click_link_by_partial_text(link.text)
-        html = browser.html
-        soup = bs(html,'html.parser')
+        soup = bs(browser.html,'html.parser')
         image = browser.find_by_text('Sample')
         image_url = image['href']
-        mars_dict.update({"title" + str(i): title, "img_url" + str(i): image_url})
+        img_title.append(title + "|" + image_url)
         browser.back()
         i += 1
+#    mars_dict.update({"img_title": img_title, "img_url": img_list})
+    mars_dict.update({"img_title": img_title})
 
+    browser.quit()
     return mars_dict
